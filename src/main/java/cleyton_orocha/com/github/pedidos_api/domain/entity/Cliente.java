5ff15dee156462b42407e0cf1e3b402c27ff6e04
Cleyton <cleytoodeoliveirarocha@gmail.com>
2023-09-17 22:17:20 -0300

    :construction: build: Criando Entidade Cliente

---
+++ src/main/java/cleyton_orocha/com/github/pedidos_api/domain/entity/Cliente.java
@@ -0,0 +1,100 @@
+package cleyton_orocha.com.github.pedidos_api.domain.entity;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Cliente implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String nome;
+
+    @Column(unique = true)
+    private String email;
+
+    private String cpfCnpj;
+
+    @JsonIgnore
+    private String senha;
+
+    public Cliente() {
+
+    }
+
+    public Cliente(Integer id, String nome, String email, String cpfCnpj, String senha) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.cpfCnpj = cpfCnpj;
+        this.senha = senha;
+    }
+
+    
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpfCnpj() {
+        return cpfCnpj;
+    }
+
+    public void setCpfCnpj(String cpfCnpj) {
+        this.cpfCnpj = cpfCnpj;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Cliente cliente = (Cliente) obj;
+        return getId().equals(cliente.getId());
+    }
+
+}
