e01b06c67de2dc1558683be7afe3c85969a2b735
Cleyton <cleytoodeoliveirarocha@gmail.com>
2023-09-21 20:58:31 -0300

    :construction: build: Relação Produto/Categoria

--- src/main/java/cleyton_orocha/com/github/pedidos_api/domain/entity/Categoria.java
+++ src/main/java/cleyton_orocha/com/github/pedidos_api/domain/entity/Categoria.java
@@ -2,16 +2,17 @@ package cleyton_orocha.com.github.pedidos_api.domain.entity;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
-import java.util.Set;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 
-public class Produto implements Serializable {
+@Entity
+public class Categoria implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
@@ -21,27 +22,11 @@ public class Produto implements Serializable {
 
     private String name;
 
-    private Double preco;
+    @ManyToMany(mappedBy = "categorias")
+    private List<Produto> produtos = new ArrayList<>();
 
-    private List<Categoria> categorias = new ArrayList<>();
-
-    private Set<ItemPedido> itens = new HashSet<>();
-
-    public Produto() {
-    }
-
-    public Produto(Integer id, String name, Double preco) {
-        this.id = id;
-        this.name = name;
-        this.preco = preco;
-    }
-
-    public List<Categoria> getCategorias() {
-        return categorias;
-    }
-
-    public Set<ItemPedido> getItens() {
-        return itens;
+    public List<Produto> getProdutos() {
+        return produtos;
     }
 
     public Integer getId() {
@@ -60,26 +45,19 @@ public class Produto implements Serializable {
         this.name = name;
     }
 
-    public Double getPreco() {
-        return preco;
-    }
-
-    public void setPreco(Double preco) {
-        this.preco = preco;
-    }
-
     @Override
     public boolean equals(Object o) {
         if (this == o)
             return true;
         if (o == null || getClass() != o.getClass())
             return false;
-        Produto produto = (Produto) o;
-        return getId().equals(produto.getId());
+        Categoria categoria = (Categoria) o;
+        return getId().equals(categoria.getId());
     }
 
     @Override
     public int hashCode() {
         return Objects.hash(getId());
     }
+
 }
