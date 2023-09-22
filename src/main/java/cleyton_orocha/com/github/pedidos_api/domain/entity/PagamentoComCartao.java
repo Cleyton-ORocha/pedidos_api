96f28451f6fbc9757930d4680abffb6ff7a395fd
Cleyton <cleytoodeoliveirarocha@gmail.com>
2023-09-22 17:35:48 -0300

    :construction: build: Tipos de Pagamentos

---
+++ src/main/java/cleyton_orocha/com/github/pedidos_api/domain/entity/PagamentoComCartao.java
@@ -0,0 +1,32 @@
+package cleyton_orocha.com.github.pedidos_api.domain.entity;
+
+import com.fasterxml.jackson.annotation.JsonTypeName;
+
+import cleyton_orocha.com.github.pedidos_api.domain.enums.EstadoPagamento;
+import jakarta.persistence.Entity;
+
+@Entity
+@JsonTypeName("pagamentoComCartao")
+public class PagamentoComCartao extends Pagamento {
+
+    private static final long serialVersionUID = 1L;
+
+    private Integer numeroDeParcelas;
+
+    public PagamentoComCartao(Integer id, EstadoPagamento estado, Pedido pedido) {
+        super(id, estado, pedido);
+    }
+
+    public PagamentoComCartao(Integer id, EstadoPagamento estado, Pedido pedido, Integer numeroDeParcelas) {
+        super(id, estado, pedido);
+        this.numeroDeParcelas = numeroDeParcelas;
+    }
+
+    public Integer getNumeroDeParcelas() {
+        return numeroDeParcelas;
+    }
+
+    public void setNumeroDeParcelas(Integer numeroDeParcelas) {
+        this.numeroDeParcelas = numeroDeParcelas;
+    }
+}
