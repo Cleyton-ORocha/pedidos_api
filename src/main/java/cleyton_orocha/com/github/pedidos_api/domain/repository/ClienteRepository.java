17bbbea07d73ff9a52261a689728853d262bc2e8
Cleyton <cleytoodeoliveirarocha@gmail.com>
2023-10-16 13:42:19 -0300

    :construction: build ClienteValidation

--- src/main/java/cleyton_orocha/com/github/pedidos_api/domain/repository/ClienteRepository.java
+++ src/main/java/cleyton_orocha/com/github/pedidos_api/domain/repository/ClienteRepository.java
@@ -1,12 +1,15 @@
 
 package cleyton_orocha.com.github.pedidos_api.domain.repository;
 
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import cleyton_orocha.com.github.pedidos_api.domain.entity.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+
+    @Transactional(readOnly = true)
+    Cliente findByEmail(String email);
 }
